219dbd4a6fedf9a9a64d01fcc0a74ef0683c19f1
navas <gabrielnavas@email.com>
2023-06-20 00:15:52 -0300

    basic struct

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+}
+
+
+struct Pendulum {
+    // This is the angle of the pendulum.
+    angle: f32,
+    
+    angular_velocity: f32,
+    angular_acceleration: f32,
+
+    r: f32, // The length of the pendulum.
+    m: f32, // The mass of the ball.
+    g: f32, // The gravity
+}
+
+impl Pendulum {
+    fn new() {
+
+    }
+
+    fn update() {
+
+    }
+
+    fn draw() {
+
+    }
+}
